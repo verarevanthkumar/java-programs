e7b70e81e7ec575f3f3f41ad64faf18cff893637
DELL <DELL@192.168.2.11>
2019-06-23 19:33:44 +0530

    #java programs

---
+++ Java/src/com/Abc/program/Firstprogram.java
@@ -0,0 +1,14 @@
+package com.Abc.program;
+
+public class Firstprogram {
+	String str="abc";
+
+	public static void main(String[] args) {
+		
+		
+		Firstprogram f=new Firstprogram();
+		System.out.println(f.str);
+
+	}
+
+}
